c870144277941460cf87db7fa1022eaef16a2cc8
Jörg Schlötterer <joerg.schloetterer@uni-passau.de>
2015-05-28 16:07:10 +0200

    minor

--- searchBar.js
+++ searchBar.js
@@ -1,5 +1,5 @@
 define(['jquery'], function($) {
-    var contentArea = $('<iframe src="' + chrome.extension.getURL('vizualization-widgets/SearchResultList/index.html') + '" style="position:fixed;width:60%;height:60%;bottom:20px;right:0px;background-color:white;border:1px solid black;z-index:99999"></div>').hide();
+    var contentArea = $('<iframe src="' + chrome.extension.getURL('visualization-widgets/SearchResultList/index.html') + '" style="position:fixed;width:60%;height:60%;bottom:20px;right:0px;background-color:white;border:1px solid black;z-index:99999"></div>').hide();
     $('body').append(contentArea);
     var bar = $('<div style="position:fixed;width:100%;height:20px;padding:5px;bottom:0;background-color:black;text-align:left;z-index:99999;"></div>');
     var form = $('<form style="display:inline;"><input id="eexcess_search" type="text" size="20" /><input type="submit" /></form>');
